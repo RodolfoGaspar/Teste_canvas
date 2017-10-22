efdefe223b685c3b7a3a8058af948f7762696214
rodolfo <dolfogaspar@gmail.com>
2017-10-22 19:51:02 -0200

    first commit

---
+++ test.rb
@@ -0,0 +1,22 @@
+# /api/v1/users/self/profile?access_token=7~qK0CtUIJ66Lfb6rJhAD3gDybSDwl2RGNOzyUfPcXNY7UQz5jQfzruC5Zs4FqLTly
+
+require 'rubygems'
+require 'json'
+
+JSON.parse("htts://canvas.instructure.com/api/v1/users/self/profile?access_token=7~qK0CtUIJ66Lfb6rJhAD3gDybSDwl2RGNOzyUfPcXNY7UQz5jQfzruC5Zs4FqLTly")
+
+string = '{"desc":{"someKey":"someValue","anotherKey":"value"},"main_item":{"stats":{"a":8,"b":12,"c":10}}}'
+parsed = JSON.parse(string) # returns a hash
+
+p parsed["desc"]["someKey"]
+p parsed["main_item"]["stats"]["a"]
+
+# Read JSON from a file, iterate over objects
+file = open("shops.json")
+json = file.read
+
+parsed = JSON.parse(json)
+
+parsed["shop"].each do |shop|
+  p shop["id"]
+end
